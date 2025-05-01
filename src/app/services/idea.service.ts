3852421f1bc337e95280aaaf03c423ae7f4d52ab
Zgjim Kabashi <zgjim.kabashi@gmail.com>
2025-05-01 23:14:51 +0200

    Adding environment as base file for routing

--- src/app/services/idea.service.ts
+++ src/app/services/idea.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { lastValueFrom, Observable } from 'rxjs';
 import { Idea } from '../interfaces/idea.interface';
 import { AddIdeaDto } from '../dtos/add-idea-dto.interface';
+import { environment } from '../assets/environments/environment.dev';
 
 @Injectable({
   providedIn: 'root',
@@ -10,23 +11,26 @@ import { AddIdeaDto } from '../dtos/add-idea-dto.interface';
 export class IdeaService {
   private readonly http = inject(HttpClient);
 
-  private apiUrl = 'https://localhost:7276/api/ideas';
+  private apiUrl = environment.apiUrl;
 
   public async getItems() {
-    return await lastValueFrom(this.http.get<Idea[]>(this.apiUrl));
+    console.log(this.apiUrl);
+    return await lastValueFrom(this.http.get<Idea[]>(`${this.apiUrl}/ideas`));
   }
 
   public async getIdeaById(id: number) {
-    const url = `${this.apiUrl}/getIdea/${id}`;
+    const url = `${this.apiUrl}/ideas/getIdea/${id}`;
     return await lastValueFrom(this.http.get<Idea>(url));
   }
 
   public async createIdea(idea: AddIdeaDto) {
-    return await lastValueFrom(this.http.post<Idea>(this.apiUrl, idea));
+    return await lastValueFrom(
+      this.http.post<Idea>(`${this.apiUrl}/ideas`, idea)
+    );
   }
 
   public async upvote(id: number) {
-    const url = `${this.apiUrl}/upvote/${id}`;
+    const url = `${this.apiUrl}/ideas/upvote/${id}`;
     return await lastValueFrom(this.http.post<Idea>(url, null));
   }
 }
